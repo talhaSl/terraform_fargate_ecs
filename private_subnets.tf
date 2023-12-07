1502053f393e943c931cfb05aaaa85b899fbfd5c
talhaSl <100585181+talhaSl@users.noreply.github.com>
2023-12-08 01:13:24 +0500

    Update private_subnets.tf

--- private_subnets.tf
+++ private_subnets.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.ecs_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-2b"
 
   tags = {
     Name = "private subnet 2"
